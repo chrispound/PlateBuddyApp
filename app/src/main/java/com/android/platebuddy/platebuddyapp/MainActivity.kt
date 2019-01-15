d40671ac64a50228f4a7bec850ebfcd36048de25
Sirchip <devsirchip@gmail.com>
2019-01-14 22:47:22 -0600

    Added initial ui

--- app/src/main/java/com/android/platebuddy/platebuddyapp/MainActivity.kt
+++ app/src/main/java/com/android/platebuddy/platebuddyapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.platebuddy.platebuddyapp
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.android.platebuddy.platebuddyapp.R.id.message
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,15 +11,15 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+//                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+//                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+//                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }

940efca872cbd35a4b435c4017c9d507f01c90c9
Sirchip <devsirchip@gmail.com>
2019-01-29 23:10:46 -0600

    Replaced display with RecyclerView, Added plate display update

--- app/src/main/java/com/android/platebuddy/platebuddyapp/main/ui/calculator/PlateCalculatorFragment.kt
+++ app/src/main/java/com/android/platebuddy/platebuddyapp/main/ui/calculator/PlateCalculatorFragment.kt
@@ -1,24 +1,24 @@
 package com.android.platebuddy.platebuddyapp.main.ui.calculator
 
-import android.app.AlertDialog
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.widget.TextViewCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.android.platebuddy.platebuddyapp.R
 import com.android.platebuddy.platebuddyapp.models.PlateResult
 import kotlinx.android.synthetic.main.fragment_plate_calculator.*
 
 
-class PlateCalculatorFragment: Fragment() {
+class PlateCalculatorFragment : Fragment() {
 
     private lateinit var viewModel: PlateCalculatorViewModel
-
+    private val viewAdapter = PlateWeightsAdapter()
+    private lateinit var viewManager: RecyclerView.LayoutManager;
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_plate_calculator, container, false)
@@ -27,8 +27,8 @@ class PlateCalculatorFragment: Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(PlateCalculatorViewModel::class.java)
-        viewModel.getPlateResult().observe(this, Observer<PlateResult>{ plateResult ->
-            displayResults(plateResult!!)
+        viewModel.getPlateResult().observe(this, Observer<PlateResult> { plateResult ->
+            viewAdapter.plates = plateResult?.plates ?: emptyList()
         })
     }
 
@@ -38,27 +38,24 @@ class PlateCalculatorFragment: Fragment() {
     }
 
     private fun setupView() {
+        viewManager = LinearLayoutManager(context)
         btnCalculate.setOnClickListener {
             val weightToLift = weightToLiftEditTxt.text.toString().toFloat()
             val barWeight = getBarWeight()
             val plateResult = viewModel.calculatePlateResult(barWeight, weightToLift)
             viewModel.setPlateResult(plateResult)
         }
-    }
 
-    private fun displayResults(plateResult: PlateResult) {
-        plateResult.plates.forEach {
-            val plateDisplay = it.count.toString() + " " + it.weight + " plates"
-            val tvPlateDisplay = TextView(this.context)
-            tvPlateDisplay.text = plateDisplay
-            TextViewCompat.setTextAppearance(tvPlateDisplay, R.style.TextAppearance_MaterialComponents_Headline5)
-            ll_plate_display.addView(tvPlateDisplay)
+        relativeLayoutPlates.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
         }
     }
 
     private fun getBarWeight(): Float {
         val currentBarWeightId = barWeightRadioGrp.checkedRadioButtonId
-        if(currentBarWeightId == R.id.barWeight45RadioBtn) {
+        if (currentBarWeightId == R.id.barWeight45RadioBtn) {
             return 45.0f
         }
         return 35.0f
---
+++ app/src/main/java/com/android/platebuddy/platebuddyapp/main/ui/calculator/PlateWeightsAdapter.kt
@@ -0,0 +1,39 @@
+package com.android.platebuddy.platebuddyapp.main.ui.calculator
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import com.android.platebuddy.platebuddyapp.R
+import com.android.platebuddy.platebuddyapp.models.Plate
+import java.text.DecimalFormat
+
+class PlateWeightsAdapter: RecyclerView.Adapter<PlateWeightsViewHolder>(){
+
+    val weightFormat = DecimalFormat("##.#")
+    var plates: List<Plate> = emptyList()
+        set(value) {
+            this.notifyDataSetChanged()
+            field = value
+        }
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): PlateWeightsViewHolder {
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_textview_weight, parent, false) as TextView
+        return PlateWeightsViewHolder(textView)
+    }
+
+    override fun getItemCount(): Int {
+        return plates.size
+    }
+
+    override fun onBindViewHolder(vh: PlateWeightsViewHolder, position: Int) {
+        val plate = plates[position]
+        val weightDispaly = weightFormat.format(plate.weight)
+        vh.textView.text = vh.itemView.context.getString(R.string.plate_display, plate.count, weightDispaly)
+    }
+
+}
+
+class PlateWeightsViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {
+
+}
